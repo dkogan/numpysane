c7a3e599190b47db25ab18300266458f92c17dd2
Dima Kogan <dima@secretsauce.net>
2019-11-30 22:13:36 -0800

    changelog bump

--- numpysane.py
+++ numpysane.py
@@ -562,7 +562,7 @@ import inspect
 from distutils.version import StrictVersion
 
 # setup.py assumes the version is a simple string in '' quotes
-__version__ = '0.20'
+__version__ = '0.21'
 
 def _product(l):
     r'''Returns product of all values in the given list'''
