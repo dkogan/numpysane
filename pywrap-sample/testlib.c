e27a072ff9ec1f0b9ef419684280550ff4a147f0
Dima Kogan <dima@secretsauce.net>
2019-12-08 00:38:01 -0800

    minor rewording

--- pywrap-sample/testlib.c
+++ pywrap-sample/testlib.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <stdint.h>
 
-// Header for a demo C library being wrapped by numpysane_pywrap. This library
-// can compute inner and outer products with arbitrary strides. The inner
-// product is implemented with 32-bit integers, 64-bit integers and 64-bit
-// floats
+// A demo C library being wrapped by numpysane_pywrap. This library can compute
+// inner and outer products with arbitrary strides. The inner product is
+// implemented with 32-bit integers, 64-bit integers and 64-bit floats
 
 
 #define DEFINE_INNER_T(T)                                               \
